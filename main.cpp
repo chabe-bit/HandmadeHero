b161aaa6072a504dc4401b01c6bad63385f310cf
chabe-bit <62448035+chabe-bit@users.noreply.github.com>
2024-01-11 12:22:54 -0800

    Created our DBISection and prepared our bitmap buffer for rendering

--- main.cpp
+++ main.cpp
@@ -1,9 +1,66 @@
 #include <Windows.h>
 
-// UINT = 32 bit unsigned integer
+#define internal static
+#define local_persist static
+#define global_variable static
+
+// Contain a variable to exit on its own
+global_variable bool Running; // Global for now
+global_variable BITMAPINFO BitmapInfo;
+global_variable void* BitmapMemory; // Memory that we receive from windows to draw into our renderer
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+// DIBSection, device independent bitmap, talk about the things you wrtie into its buffer to display using GDI
+internal void
+x64ResizeDBISection(int Width, int Height)
+{
+	// TODO(Ben): Bulletproof this
+	// Maybe don't free first, free after, then free first
+
+	// Check to see if we have a bitmaphandle, if we don't then we go ahead and create one
+	if (BitmapHandle)
+	{
+		DeleteObject(BitmapHandle);
+	}
+
+	if (!BitmapDeviceContext)
+	{
+		BitmapDeviceContext = CreateCompatibleDC(0);
+	}
+
+	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+	BitmapInfo.bmiHeader.biWidth = Width;
+	BitmapInfo.bmiHeader.biHeight = Height;
+	BitmapInfo.bmiHeader.biPlanes = 1;
+	BitmapInfo.bmiHeader.biBitCount = 32;
+	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+	BitmapInfo.bmiHeader.biSizeImage = BI_RGB;
+
+	BitmapHandle = CreateDIBSection(
+		 BitmapDeviceContext,
+		 &BitmapInfo,
+		 DIB_RGB_COLORS,
+		 &BitmapMemory,
+		 0,
+		 0);
+}
+
+internal void
+x64UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+	// Copy one rect to another
+	StretchDIBits(DeviceContext, 
+		X, Y, Width, Height,
+		X, Y, Width, Height,
+		BitmapMemory,
+		&BitmapInfo,
+		DIB_RGB_COLORS,
+		SRCCOPY
+	);
+}
 
 LRESULT CALLBACK
-MainWindowCallback(HWND Window,
+x64MainWindowCallback(HWND Window,
 					UINT Message,
 					WPARAM wParam,
 					LPARAM lParam)
@@ -15,17 +72,27 @@ MainWindowCallback(HWND Window,
 	{
 		case WM_SIZE:	 // User change window size
 		{
+			// Every time we resize our window, this function is called to correct our display to fit the window
+			RECT ClientRect;
+			GetClientRect(Window, &ClientRect);
+
+			// Find the W and H for the buffer in our window 
+			int Width = ClientRect.bottom - ClientRect.top;
+			int Height = ClientRect.right - ClientRect.left;
+			x64ResizeDBISection(Width, Height);
 			OutputDebugStringA("WN_SIZE\n");
 			break;
 		}
 		case WM_DESTROY: // Window deletes window
 		{
-			OutputDebugStringA("WM_DESTROY\n");
+			// Handle as an error, recreate window
+			Running = false;
 			break;
 		}
 		case WM_CLOSE:   // User closes the window
 		{
-			OutputDebugStringA("WM_CLOSE\n");
+			// Handle with a message to the user
+			Running = false;
 			break;
 		}
 		case WM_ACTIVATEAPP: // User clicks app to be used
@@ -38,13 +105,15 @@ MainWindowCallback(HWND Window,
 			// Prepares the window for painting
 			PAINTSTRUCT Paint;
 			HDC DeviceContext = BeginPaint(Window, &Paint);
+			
+			
 
-			// Find the WIDTH & HEIGHT
+			// Find the Width & Height
 			int X = Paint.rcPaint.left;
 			int Y = Paint.rcPaint.top;
-			int WIDTH = Paint.rcPaint.bottom - Paint.rcPaint.top;
-			int HEIGHT = Paint.rcPaint.right - Paint.rcPaint.left;
-			PatBlt(DeviceContext,X, Y, WIDTH, HEIGHT, WHITENESS); 
+			int Width = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			int Height = Paint.rcPaint.right - Paint.rcPaint.left;
+			x64UpdateWindow(DeviceContext, X, Y, Width, Height);
 
 			EndPaint(Window, &Paint);
 			break;
@@ -68,7 +137,7 @@ WinMain(HINSTANCE Instance,
 	WNDCLASSA WindowClass = {};
 
 	WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW; // Set of binary flags
-	WindowClass.lpfnWndProc = MainWindowCallback; // Pointer to a function 
+	WindowClass.lpfnWndProc = x64MainWindowCallback; // Pointer to a function 
 	WindowClass.hInstance = Instance;   // Determines what setting we set our window to
 	// WindowClass     hIcon;       
 	WindowClass.lpszClassName = "HandmadeHeroWindowClass"; // Name for our window 
@@ -94,8 +163,9 @@ WinMain(HINSTANCE Instance,
 			// Start a message loop
 			// A queue of message are created for you by the window
 			// Use GetMessage() to dispatch incoming messages
+			Running = true;
 			MSG Message;
-			for (;;)
+			while (Running)
 			{
 				BOOL MessageResult = (GetMessage(&Message, 0, 0, 0));
 				if (MessageResult > 0) // If it doesn't exit 
